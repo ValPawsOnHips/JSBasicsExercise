b76ecd16bd3f480c6bc72d90ff7e217b727e14ae
ValPawsOnHips <151215439+ValPawsOnHips@users.noreply.github.com>
2024-01-12 09:11:09 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log(13);
+
+// Numbers, Strings, Booleans, Null, and Undefined
+
+// + - * / ** %
+
+console.log(5 % 2); // 1
+console.log(5 % 4); // 1
+
+console.log('I\'M MARY POPPINS, Y\'ALL!'); // I'M MARY POPPINS, Y'ALL!
+
+console.log("Hawk" + "eye"); // Hawkeye
+
+console.log("Dakota".length + "Alarcon".length); // 13
+
+console.log("Avengers: Endgame"[16]); // e
+
+console.log("\\"); // \
+
+console.log(5 ** 4 - 9); // 616
+
+console.log("I".length - "Am".length + "Iron".length - "Man".length); // 0
+
+// AVENGERS
+console.log("CAP"[1]); // A
+console.log("VISION"[0]); // V
+console.log("OKOYE"[4]); // E
+console.log("NICK FURY"[0]); // N
+console.log("GROOT"[0]); // G
+console.log("DOCTOR STRANGE"[13]); // E
+console.log("THOR"[3]); // R
+console.log("STAR-LORD"[0]); // S
+
+var m = 25;
+var c = 4;
+var u = m * c;
+console.log(u);
